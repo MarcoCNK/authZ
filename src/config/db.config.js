17e9c536c4ccd06ebb21d76a0147e6aa07a61f03
MarcoCNK <virtualnautilus@gmail.com>
2024-12-26 22:59:48 -0300

    cve bcript

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,12 +1,10 @@
 import mongoDB from 'mongoose'
-import User from '../models/user.models.js'
 
 const MONGO_URL = 'mongodb://localhost:27017'
 
 mongoDB.connect(MONGO_URL, {})
 .then (() => {
     console.log('Database connected')
-    new User({name: 'Diego', email: 'b0lXJ@example.com', password: '123456', emailVerified: true, verificationToken: '' }).save()
 })
 .catch((err) => {
     console.log(err)
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,49 @@
-export const registerController = (req, res) => {
-    const { name, email, password } = req.body
+import Joi from 'joi'
+import ResponseBuilder from '../helpers/validation.helpers.js'
+import User from '../models/user.models.js'
 
-    // APPLY THE VALIDATIONS    
 
-    console.log("Request of post method has the body: ",req.body)
-    res.json({ status: 'OK' })
+export const registerController = async (req, res) => {
+    try {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(30).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(8).required()
+    })
+    const { error, value } = schema.validate(req.body)
+
+    const user = new User({name: value.name,
+         email: value.email,
+         password: value.password,
+         emailVerified: false,
+         verificationToken: '' })
+    await user.save()
+
+
+    if (error) {
+        const response = new ResponseBuilder()
+        .setOk(false)
+        .setStatus(500)
+        .setMessage(`Internal server error`)
+        .setPayload({
+            error
+        })
+        .build()
+        return response
+    }
+    
+    res.json({ status: 'OK', data: value })
+    const response = new ResponseBuilder()
+        .setOk(false)
+        .setStatus(200)
+        .setMessage(`OK`)
+        .setPayload({
+            value
+        })
+        .build()
+    return response
+    } catch (error) {
+        console.log("The error trpped by the catch: ")
+        console.log(error)
+    }
 }
